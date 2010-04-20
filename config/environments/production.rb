68e6ac3c3bccb369d7e2639212acfd415b617d5c
Igal Koshevoy <igal@pragmaticraft.com>
2010-04-20 11:00:59 -0700

    Altered "production" environment to display stack traces.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,9 @@
 config.cache_classes = true
 
 # Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = false
+# FIXME Temporarily allow stack traces to be visible in production
+### config.action_controller.consider_all_requests_local = false
+config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
