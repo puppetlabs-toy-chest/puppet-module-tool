ba799aa9fb36ddc9f3760e0f3c924fb0b2129117
Igal Koshevoy <igal@pragmaticraft.com>
2010-04-23 04:54:38 -0700

    Fixed ModsHelper#project_feed_link so it only renders content if it knows how to parse the feed. Added specs. Need to refactor this to avoid having to guess or have the errors vanish.

--- app/helpers/mods_helper.rb
+++ app/helpers/mods_helper.rb
@@ -1,9 +1,11 @@
 module ModsHelper
 
+  # FIXME Add validations to Mod to prevent users from adding invalid feeds that we then have to guess how to handle or how to alert users of problems.
   def project_feed_link(mod)
     unless mod.project_feed_url.blank?
-      type = %w(atom rss).detect { |t| mod.project_feed_url.include?(t) }
-      auto_discovery_link_tag(type.to_sym, mod.project_feed_url, :title => "#{mod.full_name} Puppet Module Project Feed")
+      if match = mod.project_feed_url.match(/\b(atom|rss)\b/)
+        auto_discovery_link_tag(match[1].to_sym, mod.project_feed_url, :title => "#{mod.full_name} Puppet Module Project Feed")
+      end
     end
   end
   
--- spec/helpers/mods_helper_spec.rb
+++ spec/helpers/mods_helper_spec.rb
@@ -2,10 +2,30 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ModsHelper do
 
-  #Delete this example and add some real ones or delete this file
-  it "should be included in the object returned by #helper" do
-    included_modules = (class << helper; self; end).send :included_modules
-    included_modules.should include(ModsHelper)
+  describe "project_link_feed" do
+    it "should return an ATOM feed" do
+      mod = Factory(:mod, :project_feed_url => "http://foo.bar/feed.atom")
+
+      helper.project_feed_link(mod).should =~ /<link.+atom\+xml/
+    end
+
+    it "should return a RSS feed" do
+      mod = Factory(:mod, :project_feed_url => "http://foo.bar/feed.rss")
+
+      helper.project_feed_link(mod).should =~ /<link.+rss\+xml/
+    end
+
+    it "should return nothing for an unknown feed" do
+      mod = Factory(:mod, :project_feed_url => "http://foo.bar/feed.wtf")
+
+      helper.project_feed_link(mod).should == nil
+    end
+
+    it "should return nothing if no feed" do
+      mod = Factory(:mod, :project_feed_url => nil)
+
+      helper.project_feed_link(mod).should == nil
+    end
   end
 
 end
