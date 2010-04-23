b082a227a5d53a10f6ac8eb4a66f439355c1fae0
Igal Koshevoy <igal@pragmaticraft.com>
2010-04-23 03:00:39 -0700

    Improved spec helpers, added "unauthenticated_session_path" for describing redirections of unauthenticated users.

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -4,4 +4,10 @@ module ResponseHelpers
     JSON.parse(response.body)
   end
 
+  # Returns path unauthenticated user is redirected to when accessing path
+  # protected by the #authenticate_user! filter.
+  def unauthenticated_session_path
+    return new_user_session_url(:unauthenticated => true)
+  end
+
 end
