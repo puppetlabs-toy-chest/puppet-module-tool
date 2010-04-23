3df29cea98365ba0b1dd5caee48c56a2f594ab41
Igal Koshevoy <igal@pragmaticraft.com>
2010-04-23 03:32:05 -0700

    Fixed ModsController and added specs providing almost complete code coverage: - Fixed actions to prevent security holes where anyone could do almost anything to anyone's records, added access checkers and guards. - Fixed how User and Mod records are assigned so that it's possible to detect errors. - Fixed actions so they don't raise exceptions when called with non-existent records, added error checkers and guards. - Added destroy feature.

--- app/controllers/mods_controller.rb
+++ app/controllers/mods_controller.rb
@@ -1,13 +1,20 @@
 class ModsController < ApplicationController
 
-  before_filter :find_user
-  before_filter :find_mod, :only => [:show, :edit, :update, :destroy]
+  before_filter :assign_user, :only => [:index]
+  before_filter :assign_user_or_redirect, :only => [:show, :edit, :update, :destroy]
+  before_filter :assign_mod_or_redirect, :only => [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
-  
+  before_filter :authorize_change_or_redirect, :only => [:edit, :update, :destroy]
+
   def index
+    if @user_assigned_status == false
+      notify_of :error, "Could not find user '#{params[:user_id]}'"
+    end
     @mods = search_scope
     respond_to do |format|
-      format.json { render :json => serialize(@mods) }
+      format.json do
+        render :json => serialize(@mods)
+      end
       format.html do
         @mods = @mods.paginate :page => params[:page], :order => 'name DESC'
       end
@@ -50,15 +57,28 @@ class ModsController < ApplicationController
       render :action => 'edit'
     end
   end
-  
+
+  def destroy
+    @mod.destroy
+    notify_of "Destroyed module"
+    redirect_to vanity_path(@mod.owner)
+  end
+
   private
-  
+
+  #===[ Utilities ]=======================================================
+
+  # Return records for all users, a single user, or a search query on either.
   def search_scope
-    if params[:q]
-      Mod.with_releases.matching(params[:q])
-    else
-      Mod.with_releases
-    end
+    base = \
+      @user ?
+      @user.mods :
+      Mod
+
+    return \
+      params[:q] ?
+      base.with_releases.matching(params[:q]) :
+      base.with_releases
   end
 
   # Serialize one or more modules to JSON
@@ -69,14 +89,55 @@ class ModsController < ApplicationController
                 )
   end
 
-  def find_user
+  #===[ Helpers ]=========================================================
+
+  # Is the current user allowed to change this record?
+  def can_change?
+    return(@mod && current_user && @mod.owner == current_user)
+  end
+  helper_method :can_change?
+
+  #===[ Filters ]=========================================================
+
+  # Assign a @user record and @user_assigned_status.
+  #
+  # The @user_assigned_status values are:
+  # * true => User was specified by request and found.
+  # * false => User was specified by request but not found.
+  # * nil => User was not specified by request and is thus not set.
+  def assign_user
     if params[:user_id]
       @user = User.find_by_username(params[:user_id])
+      @user_assigned_status = @user.present?
+    else
+      @user = nil
+      @user_assigned_status = nil
     end
   end
 
-  def find_mod
-    @mod = @user.mods.find_by_name(params[:id])
+  # Assign a @user record or redirect with an error.
+  def assign_user_or_redirect
+    assign_user if @user_assigned_status.nil?
+    unless @user
+      notify_of :error, "Could not find user '#{params[:user_id]}'"
+      redirect_back_or_to mods_path
+    end
   end
-  
+
+  # Assign a @mod record or redirect with an error.
+  def assign_mod_or_redirect
+    unless @mod = @user.mods.find_by_name(params[:id])
+      notify_of :error, "Could not find module '#{params[:id]}'"
+      redirect_back_or_to vanity_path(@user)
+    end
+  end
+
+  # Only allow owner to change this record, else redirect with an error.
+  def authorize_change_or_redirect
+    unless can_change?
+      notify_of :error, "Access denied, you must be the owner of this module to change it"
+      redirect_back_or_to module_path(@mod.owner, @mod)
+    end
+  end
+
 end
--- spec/controllers/mods_controller_spec.rb
+++ spec/controllers/mods_controller_spec.rb
@@ -4,11 +4,68 @@ describe ModsController do
 
   describe "#index" do
 
+    context "with users and modules" do
+
+      before do
+        @user1 = Factory(:user)
+        @user2 = Factory(:user)
+
+        @user1mod_with_release    = Factory(:mod, :name => 'foo', :owner => @user1)
+        @user2mod_with_release    = Factory(:mod, :name => 'bar', :owner => @user2)
+        @user2mod_without_release = Factory(:mod, :name => 'baz', :owner => @user2)
+
+        @mod1rel1 = Factory(:release, :mod => @user1mod_with_release)
+        @mod2rel1 = Factory(:release, :mod => @user2mod_with_release)
+      end
+
+      context "for all mods" do
+
+        it "should render all mods with releases for all users" do
+          get :index
+
+          response.should be_success
+          assigns[:mods].should include @user1mod_with_release
+          assigns[:mods].should include @user2mod_with_release
+          assigns[:mods].should_not include @user2mod_without_release
+        end
+
+        it "should render mods with releases for all users matching search" do
+          get :index, :q => "foo"
+
+          response.should be_success
+          assigns[:mods].should == [@user1mod_with_release]
+        end
+
+      end
+
+      context "for user's mods" do
+
+        it "should render all mods with releases for a user" do
+          get :index, :user_id => @user1.name, :format => "html"
+
+          response.should be_success
+          assigns[:mods].should include @user1mod_with_release
+          assigns[:mods].should_not include @user2mod_with_release
+          assigns[:mods].should_not include @user2mod_without_release
+        end
+
+        it "should display error if user doesn't exist" do
+          get :index, :user_id => "invalid_user_name"
+
+          response.should be_success
+          assigns[:user].should be_nil
+          flash[:error].should_not be_blank
+        end
+
+      end
+
+    end
+
     context "via JSON, querying" do
 
       before do
         @user = Factory(:user)
-        @mod1 = Factory(:mod, :name => 'foo', :owner_id => @user.id, :owner_type => 'User')
+        @mod1 = Factory(:mod, :name => 'foo', :owner => @user)
         get :index
       end
 
@@ -22,5 +79,253 @@ describe ModsController do
 
   end
 
+  describe "#new" do
+    it "should show new record form when logged-in" do
+      @user = Factory(:user)
+
+      sign_in @user
+      get :new
+
+      response.should be_success
+      assigns[:mod].should be_a_new_record
+    end
+
+    it "should not allow new record form unless logged-in" do
+      get :new
+
+      response.should be_redirect
+      assigns[:mod].should be_nil
+    end
+  end
+
+  describe "#create" do
+    context "when logged-in" do
+      before do
+        @user = Factory(:user)
+        sign_in @user
+      end
+
+      it "should create a record" do
+        attributes = Factory.attributes_for(:mod, :owner => nil)
+
+        post :create, "mod" => attributes
+
+        response.should be_redirect
+        flash[:error].should be_nil
+        assigns[:mod].should be_valid
+      end
+
+      it "should not create an invalid record" do
+        attributes = Factory.attributes_for(:mod, :owner => nil, :name => nil)
+
+        post :create, "mod" => attributes
+
+        response.should be_success
+        flash[:error].should_not be_blank
+        assigns[:mod].should_not be_valid
+      end
+    end
+
+    it "should not allow anonymous user to create a record" do
+
+    end
+  end
+
+  describe "#edit" do
+    before do
+      @user1 = Factory(:user)
+      @user1mod = Factory(:mod, :owner => @user1)
+
+      @user2 = Factory(:user)
+      @user2mod = Factory(:mod, :owner => @user2)
+    end
+
+    context "when logged-in" do
+      it "should allow owner to edit their module" do
+        sign_in @user1
+        get :edit, :user_id => @user1.name, :id => @user1mod.name
+
+        response.should be_success
+        assigns[:mod].should == @user1mod
+      end
+
+      it "should not allow a user to edit another's module" do
+        sign_in @user2
+        get :edit, :user_id => @user1.name, :id => @user1mod.name
+
+        response.should be_redirect
+        flash[:error].should_not be_blank
+      end
+
+      it "should display error if trying to edit an invalid module" do
+        sign_in @user1
+        get :edit, :user_id => @user1.name, :id => "invalid_module_name"
+
+        response.should be_redirect
+        flash[:error].should_not be_blank
+      end
+    end
+
+    context "when anonymous" do
+      it "should not allow anonymous user to edit a module" do
+        get :edit, :user_id => @user1.name, :id => @user1mod.name
+
+        response.should be_redirect
+        flash[:error].should be_blank
+      end
+    end
+  end
+
+  describe "#update" do
+    before do
+      @user1 = Factory(:user)
+      @user1mod = Factory(:mod, :owner => @user1)
+
+      @user2 = Factory(:user)
+      @user2mod = Factory(:mod, :owner => @user2)
+
+      @attributes = Factory.attributes_for(:mod, :owner => nil)
+    end
+
+    context "when logged-in" do
+      it "should allow owner to update their module" do
+        sign_in @user1
+        put :update, :user_id => @user1.name, :id => @user1mod.name, :mod => @attributes
+
+        response.should be_redirect
+        flash[:error].should be_blank
+        assigns[:mod].should be_valid
+        assigns[:mod].should == @user1mod
+        assigns[:mod].name.should == @attributes[:name]
+      end
+
+      it "should display error if update was invalid" do
+        attributes = @attributes.dup
+        attributes[:name] = "!" # Name violates validation rules
+
+        sign_in @user1
+        put :update, :user_id => @user1.name, :id => @user1mod.name, :mod => attributes
+
+        response.should be_success
+        flash[:error].should_not be_blank
+        assigns[:mod].should_not be_valid
+        assigns[:mod].should == @user1mod
+        assigns[:mod].name.should == attributes[:name]
+      end
+
+      it "should not allow a user to update another's module" do
+        sign_in @user2
+        get :update, :user_id => @user1.name, :id => @user1mod.name
+
+        response.should be_redirect
+        flash[:error].should_not be_blank
+      end
+
+      it "should display error if trying to update an invalid module" do
+        sign_in @user1
+        get :update, :user_id => @user1.name, :id => "invalid_module_name"
+
+        response.should be_redirect
+        flash[:error].should_not be_blank
+      end
+    end
+
+    context "when anonymous" do
+      it "should not allow anonymous user to edit a module" do
+        get :edit, :user_id => @user1.name, :id => @user1mod.name
+
+        response.should be_redirect
+        flash[:error].should be_blank
+      end
+    end
+  end
+
+  describe "#show" do
+    it "should show a user's module that has releases" do
+      @user = Factory(:user)
+      @mod1 = Factory(:mod, :name => 'foo', :owner => @user)
+      @release1 = Factory(:release, :mod => @mod1)
+
+      get :show, :user_id => @user.name, :id => @mod1.name
+
+      response.should be_success
+      assigns[:user].should == @user
+      assigns[:mod].should == @mod1
+      flash[:error].should be_blank
+    end
+
+    it "should show a user's module that has no releases" do
+      @user = Factory(:user)
+      @mod1 = Factory(:mod, :name => 'foo', :owner => @user)
+
+      get :show, :user_id => @user.name, :id => @mod1.name
+
+      response.should be_success
+      assigns[:user].should == @user
+      assigns[:mod].should == @mod1
+      flash[:error].should be_blank
+    end
+
+    it "should display error if module doesn't exist" do
+      @user = Factory(:user)
+
+      get :show, :user_id => @user.name, :id => "invalid_module_name"
+
+      response.should redirect_to(vanity_path(@user))
+      assigns[:user].should == @user
+      assigns[:mod].should be_nil
+      flash[:error].should_not be_blank
+    end
+
+    it "should display error if user doesn't exist" do
+      get :show, :user_id => "invalid_user_name", :id => "invalid_module_name"
+
+      response.should redirect_to(mods_path)
+      assigns[:user].should be_nil
+      assigns[:mod].should be_nil
+      flash[:error].should_not be_blank
+    end
+
+    describe "#edit" do
+    end
+  end
+
+  describe "#destroy" do
+    before do
+      @user1 = Factory(:user)
+      @user1mod = Factory(:mod, :owner => @user1)
+
+      @user2 = Factory(:user)
+    end
+
+    context "when logged-in" do
+      it "should allow a user to delete their module" do
+        sign_in @user1
+
+        delete :destroy, :user_id => @user1.name, :id => @user1mod.name
+
+        response.should redirect_to(vanity_path(@user1))
+        Mod.exists?(@user1mod.id).should be_false
+      end
+
+      it "should not allow a user to delete another's module" do
+        sign_in @user2
+
+        delete :destroy, :user_id => @user1.name, :id => @user1mod.name
+
+        response.should redirect_to(module_path(@user1, @user1mod))
+        Mod.exists?(@user1mod.id).should be_true
+      end
+    end
+
+    context "when anonymous" do
+      it "should not allow destroy" do
+        delete :destroy, :user_id => @user1.name, :id => @user1mod.name
+
+        response.should redirect_to(unauthenticated_session_path)
+        Mod.exists?(@user1mod.id).should be_true
+      end
+    end
+  end
 
 end
