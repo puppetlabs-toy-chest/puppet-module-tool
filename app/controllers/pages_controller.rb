347e345a963b553540f82560ddafbecd520a1d22
Igal Koshevoy <igal@pragmaticraft.com>
2010-05-05 23:28:44 -0700

    Fixed pages#home to not redirect user to their page because this prevents them from being able to view the homepage.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,2 @@
 class PagesController < ApplicationController
-  before_filter :redirect_users, :only => :home
-
-  private
-
-  def redirect_users
-    if current_user
-      redirect_to user_path(current_user)
-    end
-  end
-  
 end
