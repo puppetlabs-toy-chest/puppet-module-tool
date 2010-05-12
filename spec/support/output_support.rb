af3c0d9b46bb6db17eb2865794ab283bf98fb250
Igal Koshevoy <igal@pragmaticraft.com>
2010-05-12 11:12:56 -0700

    Improved test environment, added #output_for to capture console output for block.

---
+++ spec/support/output_support.rb
@@ -0,0 +1,19 @@
+# Return string containing the STDIN and STDOUT output emitted within
+# the +block+.
+def output_for(&block)
+  output = StringIO.new
+
+  stdout_old = $stdout
+  stderr_old = $stderr
+
+  $stdout = output
+  $stderr = output
+
+  block.call
+  
+  output.rewind
+  return output.read
+ensure
+  $stdout = stdout_old
+  $stderr = stderr_old
+end
