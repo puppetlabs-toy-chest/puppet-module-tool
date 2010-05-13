57af6debe3f93225532fa89641edb527e2c6669c
Igal Koshevoy <igal@pragmaticraft.com>
2010-05-13 01:07:58 -0700

    Reworked Utils::Settings#prepare_settings to use default Puppet module repository URL.

--- lib/puppet/module/tool/utils/settings.rb
+++ lib/puppet/module/tool/utils/settings.rb
@@ -1,24 +1,29 @@
 module Puppet::Module::Tool::Utils::Settings
-
+  
   def prepare_settings(options = {})
     return if @settings_prepared
+
     if options[:config]
       Puppet.settings.send(:set_value, :config, options[:config], :cli)
     end
+
     Puppet.setdefaults(:pmt,
-                       :modulerepository => ['https://modules.puppetlabs.com',
-                                             "The module repository"],
-                       :pmtdir => ['$vardir/pmt', "The directory in which module tool data is stored"])
+      :modulerepository => [Puppet::Module::Tool::REPOSITORY_URL, "The module repository"],
+      :pmtdir => ['$vardir/pmt', "The directory in which module tool data is stored"])
+
     # Create directory for pmt's use inside ~/.puppet
     Puppet::Module::Tool.pmtdir.mkpath
 
     Puppet.settings.use(:pmt)
+
     Puppet.settings.parse
+
     [:modulerepository].each do |key|
       if options[key]
         Puppet.settings.send(:set_value, key, options[key], :cli)
       end
     end
+
     @settings_prepared = true
   end
   
