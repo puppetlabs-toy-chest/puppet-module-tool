b4257a6946ba64aa2b795ce51451beed0821346c
Igal Koshevoy <igal@pragmaticraft.com>
2010-05-13 17:57:52 -0700

    Improved test environment, added #should_be_logged_in_as and #should_not_be_logged_in.

--- spec/support/devise_helpers.rb
+++ spec/support/devise_helpers.rb
@@ -1,3 +1,13 @@
 class ActionController::TestCase
   include Devise::TestHelpers
+
+  # Ensure the user is logged in as the specified +user+.
+  def should_be_logged_in_as(user)
+    controller.warden.authenticate(:scope => :user).should == user
+  end
+
+  # Ensure that the user isn't logged in.
+  def should_not_be_logged_in
+    controller.warden.authenticate(:scope => :user).should == nil
+  end
 end
